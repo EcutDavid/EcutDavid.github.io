a248940fa7773b05eb7eb311ff0bd986d45c398a
David Guan <DavidGuanDev@gmail.com>
2016-06-08 08:46:05 +0800

    Change style import order.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import 'normalize.css/normalize.css'
 
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import About from 'components/About'
 import BLogs from 'components/BLogs'
-import 'styles/App.scss'
 
 class AppComponent extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/Main'
+
+import 'normalize.css/normalize.css'
 import 'font-awesome/css/font-awesome.min.css'
+import 'styles/App.scss'
+
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'))
