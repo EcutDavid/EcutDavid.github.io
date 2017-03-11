a151c4d3232f0c77a840b130d5503de7c2cb0f74
David Guan <DavidGuanDev@Gmail.com>
2017-03-11 23:07:06 +0800

    remove open module

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('core-js/fn/object/assign')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
-const open = require('open')
 
 new WebpackDevServer(webpack(config), config.devServer)
 .listen(config.port, 'localhost', (err) => {
@@ -13,5 +12,4 @@ new WebpackDevServer(webpack(config), config.devServer)
   }
   console.log('Listening at localhost:' + config.port)
   console.log('Opening your system browser...')
-  open('http://localhost:' + config.port + '/webpack-dev-server/')
 })
