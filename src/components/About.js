9ed0033dc635c2f90056d51baa7ef3807228b4c1
David Guan <DavidGuanDev@Gmail.com>
2017-03-21 21:18:33 +0800

    Making typing effects

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,74 @@ import { ABOUT_ME } from 'constants/domID'
 import Works from './Works'
 import 'styles/About.scss'
 
+const DESCRIPTION_LIST = [
+  'Currently working as a developer at Splunk.',
+  'Have experience in building websites and single page applications with React, Redux and node.js.',
+  'Enjoy writing clean code and solving complex problems.',
+  'Built some back-end services in C#, node.js with AWS services.'
+];
 
 export default class About extends Component {
+  constructor() {
+    super();
+    this.state = {
+      descriptionList: DESCRIPTION_LIST.map(d => ({
+        text: d,
+        pointer: 0
+      }))
+    };
+  }
+
+  incrementDescriptionText() {
+    const { descriptionList } = this.state;
+    let incremented = false;
+    descriptionList.forEach((d) => {
+      if (incremented) {
+        return;
+      }
+      if (d.text.length > d.pointer) {
+        d.pointer++;
+        incremented = true;
+      }
+    });
+    if (incremented) {
+      this.setState({descriptionList});
+      setTimeout(() => {
+        this.incrementDescriptionText();
+      }, 40);
+    }
+  }
+
+  componentDidMount() {
+    this.incrementDescriptionText();
+  }
+
+  mapDescriptionList() {
+    const { descriptionList } = this.state;
+    return descriptionList.map(({text, pointer}, i) => {
+      const length = text.length;
+      const isDone = (i === (descriptionList.length - 1)) &&
+        pointer === length;
+      return pointer > 0 && (
+        <div key={i}>
+          <p>
+            {text.slice(0, pointer)}
+          </p>
+          {((pointer < length) || isDone) && <span className={`typing-cursor ${isDone ? 'done' : ''}`} />}
+        </div>
+      )
+    });
+  }
+
   render() {
     return (
       <div className="About row" id={ABOUT_ME}>
         <div className='column small-11 small-centered'>
           <div className='row'>
-            <div className='column small-10 small-centered'>
-              <p>Currently working as a developer at <a href='https://www.splunk.com/' target='_blank'>Splunk</a>.</p>
-              <p>Have experience in building websites and single page applications with <b>React</b>, <b>Redux</b> and <b>node.js</b>.</p>
-              <p>Enjoy writing clean code and solving complex problems.</p>
-              <p>Built some back-end services in <b>C#, node.js</b> with <b>AWS services</b>.</p>
+            <div className='column small-10 small-centered description-list'>
+              {
+                this.mapDescriptionList()
+              }
             </div>
           </div>
           <div className='row'>
@@ -58,4 +114,3 @@ export default class About extends Component {
     )
   }
 }
-// <div className="column small-10 medium-6 medium-uncentered large-uncentered small-centered large-4">
