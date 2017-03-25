e38f8af254d9df6b3f5d93ddb71eaaf19d96479e
David Guan <DavidGuanDev@Gmail.com>
2017-03-25 10:56:32 +0800

    Improve wording

--- src/constants/worksData.js
+++ src/constants/worksData.js
@@ -14,8 +14,8 @@ export default [{
   description: (
       <div>
         <p>My first project in Wiredcraft.</p>
-        <p>Implemented the requirements in front-end and improved the performance of application.</p>
-        <p>View the <a href='http://data.worldbank.org/' target='_blank'>website</a>.</p>
+        <p>Implemented the requirements in front-end and improved the performance.</p>
+        <a href='http://data.worldbank.org/' target='_blank'>Production website</a>
       </div>
     )
   }, {
@@ -23,8 +23,9 @@ export default [{
     picture: cncPic,
     description: (
       <div>
-        <p>Aim at control CNC(Computer Numerical Control) card which is plugged in PC.</p>
-        <p><b>C#, WPF and Open GL</b>. Source code in <a href='https://github.com/EcutDavid/eCut-CNC-software' target='_blank'>Github repo</a>.</p>
+        <p>A desktop application for control CNC cards which plugged in PC.</p>
+        <p><b>C#, WPF and Open GL</b></p>
+        <a href='https://github.com/EcutDavid/eCut-CNC-software' target='_blank'>Github repo</a>
       </div>
     )
   }, {
@@ -32,8 +33,9 @@ export default [{
     picture: dv20Pic,
     description: (
       <div>
-        <p>Data visualization practises.</p>
-        <p><b>React.js, d3.js</b>. Source code in <a href='https://github.com/EcutDavid/D3In20Days' target='_blank'>Github repo</a>.</p>
+        <p>Data visualization practice.</p>
+        <p><b>React.js, d3.js</b></p>
+        <a href='https://github.com/EcutDavid/D3In20Days' target='_blank'>Github repo</a>
       </div>
     )
   }, {
@@ -41,8 +43,9 @@ export default [{
     picture: b_3d,
     description: (
       <div>
-        <p>built with <b>three.js</b>, Source code in <a href='https://github.com/EcutDavid/bounce-balls-3D' target='_blank'>Github repo</a>.</p>
-        <p>View the <a href='http://davidguan.me/bounce-balls-3D/' target='_blank'>demo website</a>.</p>
+        <p>Built with <b>three.js</b>.</p>
+        <p><a href='https://github.com/EcutDavid/bounce-balls-3D' target='_blank'>Github repo</a></p>
+        <a href='http://davidguan.me/bounce-balls-3D/' target='_blank'>Online demo</a>
       </div>
     )
   }, {
@@ -51,7 +54,8 @@ export default [{
     description: (
       <div>
         <p>User authentication, post submit, up & down vote, and comments.</p>
-        <p><b>React.js, node.js, Redux.js</b>. Source code in <a href='https://github.com/EcutDavid/simple-reddit' target='_blank'>Github repo</a>.</p>
+        <p><b>React.js, node.js, Redux.js</b></p>
+        <a href='https://github.com/EcutDavid/simple-reddit' target='_blank'>Github repo</a>
       </div>
     )
   }, {
@@ -59,8 +63,8 @@ export default [{
     picture: xkcd,
     description: (
       <div>
-        <p>All of the comics from xkcd.</p>
-        <p><b>Golang, React.js</b>. Source code in <a href='https://github.com/EcutDavid/react-native-xkcdReader' target='_blank'>Github repo</a>.</p>
+        <p><b>Golang</b>, <b>React Native</b></p>
+        <a href='https://github.com/EcutDavid/react-native-xkcdReader' target='_blank'>Github repo</a>
       </div>
     )
   }
