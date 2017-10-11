fafc50368b26e68528daba485c207d0d8b64f732
David Guan <davidguandev@gmail.com>
2017-10-11 20:35:24 +1100

    React 16.0

--- cfg/dev.js
+++ cfg/dev.js
@@ -23,7 +23,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'react-hot!babel-loader',
+  loader: 'react-hot-loader/webpack!babel-loader',
   include: [].concat(
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
