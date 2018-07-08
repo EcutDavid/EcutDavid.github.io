5ca8ee0e8c6c7ee55002b54109b5fa89ab0fc613
David Guan <davidguan@canva.com>
2018-07-08 22:30:32 +1000

    claim

--- src/index.js
+++ src/index.js
@@ -8,12 +8,33 @@ import 'normalize.css/normalize.css'
 import 'font-awesome/css/font-awesome.min.css'
 import 'styles/App.scss'
 
+function toggleSnackBar() {
+  setTimeout(() => {
+    snackBar.style.opacity = 1;
+    setTimeout(() => {
+      snackBar.style.opacity = 0;
+    }, 10000)
+  }, 3000);
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js')
     .catch(function (error) {
       console.log('Service worker registration failed with ' + error);
     });
+
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    navigator.serviceWorker.controller.addEventListener('statechange', event => {
+      if (event.target.state) {
+        const isActivated = event.target.state;
+        if (isActivated) {
+          toggleSnackBar();
+        }
+      }
+    })
+  })
 }
 
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'))
+const snackBar = document.querySelector('.snack-bar');
