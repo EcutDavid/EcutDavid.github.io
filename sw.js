14cb27b912409150034b49a64725e124b195541c
David Guan <davidguandev@gmail.com>
2018-07-09 21:11:06 +1000

    better sw

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
+const cacheName = 'v2';
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v2').then(function (cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll([
         '/',
         '/assets/136133c6ea2e29e457cea61fbbc06ad3.png',
@@ -20,22 +21,31 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
+  if (!fetch) return;
+  // Don't care about analytic stuff when the user it's offline.
+  // For now... :)
+  if (event.request.url.indexOf('analytics') > -1) return;
   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
     // This is a case caused by devtools, no worries
     return;
   }
 
-  if (fetch) {
-    event.respondWith(
-      fetch(event.request).catch(function () {
-        console.log('network is in offline state, trying to providing cached offline content for:', event.request.url);
-        // Only use cache as a fallback
-        return caches.open('v2').then(cache => {
-          return cache.match(event.request);
-        });
-      })
-    );
-  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response =>
+        caches.open(cacheName).then(cache =>
+          cache.put(event.request, response.clone())
+        ).then(() => response)
+      )
+      .catch(() =>
+        // Use cache as a fallback
+        caches.open(cacheName).then(cache =>
+          cache.match(event.request).then(response =>
+            response || Promise.reject('no-match')
+          )
+        )
+      )
+  );
 });
 
 self.addEventListener('activate', function (event) {
