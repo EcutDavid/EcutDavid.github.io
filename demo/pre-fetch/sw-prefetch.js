4c6def9e3a1e923dfcb5ebe6dbd627c428f23c19
David Guan <davidguan@canva.com>
2018-07-12 19:04:32 +1000

    .

--- demo/pre-fetch/sw-prefetch.js
+++ demo/pre-fetch/sw-prefetch.js
@@ -3,36 +3,24 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll([
-        '/demo/pre-fetch/'
+        '/demo/pre-fetch/page1/',
+        '/demo/pre-fetch/page1/main.js',
+        '/demo/pre-fetch/page2/'
       ]);
     })
   );
 });
 
-// self.addEventListener('fetch', function (event) {
-//   if (!fetch) return;
-//   // Don't care about analytic stuff when the user it's offline.
-//   // For now... :)
-//   if (event.request.url.indexOf('analytics') > -1) return;
-//   if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-//     // This is a case caused by devtools, no worries
-//     return;
-//   }
-
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(response =>
-//         caches.open(cacheName).then(cache =>
-//           cache.put(event.request, response.clone())
-//         ).then(() => response)
-//       )
-//       .catch(() =>
-//         // Use cache as a fallback
-//         caches.open(cacheName).then(cache =>
-//           cache.match(event.request).then(response =>
-//             response || Promise.reject('no-match')
-//           )
-//         )
-//       )
-//   );
-// });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() =>
+        // Use cache as a fallback
+        caches.open(cacheName).then(cache =>
+          cache.match(event.request).then(response =>
+            response || Promise.reject('no-match')
+          )
+        )
+      )
+  );
+});
