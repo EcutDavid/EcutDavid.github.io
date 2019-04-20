d7c667b66b4ee1d769cf322216c09cbe22e6ace7
David Guan <davidguandev@gmail.com>
2019-04-20 16:17:27 +1000

    update

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import data from 'constants/articles'
-import { ARTICLES } from 'constants/domID'
-import 'styles/Articles.scss'
+import data from "constants/articles";
+import { ARTICLES } from "constants/domID";
+import "styles/Articles.scss";
 
 const DATE_NOW = new Date(Date.now());
 function getDateDiff(oldDate) {
@@ -10,51 +10,57 @@ function getDateDiff(oldDate) {
   const timeDiff = Math.abs(DATE_NOW.getTime() - date1.getTime());
   const days = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-  return `${days} day${days > 1 ? 's' : ''} ago`;
+  return `${days} day${days > 1 ? "s" : ""} ago`;
 }
 
 export default class Articles extends Component {
   constructor() {
-    super()
-    this.state={
+    super();
+    this.state = {
       showActionButton: true,
       showAll: false
-    }
+    };
   }
 
   toggleShowAll() {
-    const { showAll } = this.state
+    const { showAll } = this.state;
 
     this.setState({
       showAll: !showAll
     });
     setTimeout(() => {
-      this.setState({showActionButton: false});
+      this.setState({ showActionButton: false });
     }, 0);
   }
 
   render() {
-    const { showAll, showActionButton } = this.state
+    const { showAll, showActionButton } = this.state;
     return (
-      <div id={ARTICLES} className='articles-container'>
-        <h2 className='title'>Articles</h2>
-        <div className='content'>
-        {
-          data.slice(0, showAll ? Number.MAX_VALUE : 5).map((d, i) =>
-            <article className='flex-container' key={i} aria-label={`${d.name}, ${getDateDiff(d.date)}`}>
-                <div className='small-11 small-centered medium-9 large-7'>
-                  <div className="article">
-                    <h3><a className='link' target='_blank' href={d.url}>{d.name}</a></h3>
-                    <span className='date'>{getDateDiff(d.date)}</span>
-                  </div>
+      <div id={ARTICLES} className="articles-container">
+        <h2 className="title">Articles</h2>
+        <div className="content">
+          {data.slice(0, showAll ? Number.MAX_VALUE : 5).map((d, i) => (
+            <article
+              className="flex-container"
+              key={i}
+              aria-label={`${d.name}, ${d.date}`}
+            >
+              <div className="small-11 small-centered medium-9 large-7">
+                <div className="article">
+                  <h3>
+                    <a className="link" target="_blank" href={d.url}>
+                      {d.name}
+                    </a>
+                  </h3>
+                  <span className="date">{d.date}</span>
                 </div>
+              </div>
             </article>
-          )
-        }
+          ))}
         </div>
         {showActionButton && (
           <button
-            className={`button action-button ${showAll ? 'hide-button' : ''}`}
+            className={`button action-button ${showAll ? "hide-button" : ""}`}
             disabled={showAll ? true : false}
             onClick={() => this.toggleShowAll()}
           >
@@ -62,6 +68,6 @@ export default class Articles extends Component {
           </button>
         )}
       </div>
-    )
+    );
   }
 }
--- src/constants/articles.js
+++ src/constants/articles.js
@@ -1,75 +1,77 @@
-
-export default [{
-  date: 'Jan 1, 2019',
-  url: 'https://medium.com/p/8f2c498fcc7e',
-  name: '2018 Year in Review'
-}, {
-  date: 'Dec 22, 2018',
-  url: 'https://medium.com/p/1ef024e85c22',
-  name: 'AOC 2018 Day 20: A Regular Map'
-},
-{
-  date: 'June 17, 2018',
-  name: 'Fixed a bunch of accessibility issues for my website(and how)',
-  url: 'https://medium.com/p/bbaac48d314f'
-},
-{
-  date: 'June 3, 2018',
-  name: 'A bash script that saves me from git -m “I forgot git add”',
-  url: 'https://medium.com/p/434b8af95311'
-},
-{
-  date: 'May 26, 2018',
-  name: 'Participating Google Code Jam(the First Time)',
-  url: 'https://medium.com/p/67bba2d6b7f0'
-},
-{
-  date: 'May 6, 2018',
-  name: 'Introduction to Dynamic Programming with Examples',
-  url: 'https://medium.com/p/bc04dca3ccee'
-},
-{
-  date: 'Jan 29, 2018',
-  name: 'Haskell Study Note 01, Sorting',
-  url: 'https://medium.com/p/46b49648e084'
-},
-{
-  date: 'Nov 18, 2017',
-  name: 'Alpha Blending and WebGL',
-  url: 'https://medium.com/p/823d86de00d8'
-},
-{
-  date: 'Oct 16, 2017',
-  name: 'Build ARKit Application with Unity',
-  url: 'https://medium.com/p/10af4a5e3b05'
-},
-{
-  date: 'Sep 9, 2017',
-  name: 'Build Particles Effect with three.js or PixiJS',
-  url: 'https://medium.com/p/7814e154bd52'
-},
-{
-  date: 'Sep 3, 2017',
-  name: 'Build an Online Photo Filter Application',
-  url: 'https://medium.com/p/d6e00c36a3bb'
-},
-{
-  date: 'Aug 27, 2017',
-  name: 'Build a flappy bird copy with TypeScript & Pixi.js',
-  url: 'https://medium.com/p/890f5a07931e'
-},
-{
-  date: 'Aug 20, 2017',
-  name: 'WebGL and Image Filter 101',
-  url: 'https://medium.com/p/5017b290d02f'
-},
-{
-  date: 'Jul 10, 2016',
-  name: 'Build a Tic Tac Toe with React Native',
-  url: 'https://medium.com/p/2a3c44e697f2'
-},
-{
-  date: 'Jul 9, 2016',
-  name: 'Let’s build a React Native application',
-  url: 'https://medium.com/p/4190839a4f5c'
-}]
+export default [
+  {
+    date: "Jan 2019",
+    url: "https://medium.com/p/8f2c498fcc7e",
+    name: "2018 Year in Review"
+  },
+  {
+    date: "Dec 2018",
+    url: "https://medium.com/p/1ef024e85c22",
+    name: "AOC 2018 Day 20: A Regular Map"
+  },
+  {
+    date: "June 2018",
+    name: "Fixed a bunch of accessibility issues for my website(and how)",
+    url: "https://medium.com/p/bbaac48d314f"
+  },
+  {
+    date: "June 2018",
+    name: "A bash script that saves me from git -m “I forgot git add”",
+    url: "https://medium.com/p/434b8af95311"
+  },
+  {
+    date: "May 2018",
+    name: "Participating Google Code Jam(the First Time)",
+    url: "https://medium.com/p/67bba2d6b7f0"
+  },
+  {
+    date: "May 2018",
+    name: "Introduction to Dynamic Programming with Examples",
+    url: "https://medium.com/p/bc04dca3ccee"
+  },
+  {
+    date: "Jan 2018",
+    name: "Haskell Study Note 01, Sorting",
+    url: "https://medium.com/p/46b49648e084"
+  },
+  {
+    date: "Nov 2017",
+    name: "Alpha Blending and WebGL",
+    url: "https://medium.com/p/823d86de00d8"
+  },
+  {
+    date: "Oct 2017",
+    name: "Build ARKit Application with Unity",
+    url: "https://medium.com/p/10af4a5e3b05"
+  },
+  {
+    date: "Sep 2017",
+    name: "Build Particles Effect with three.js or PixiJS",
+    url: "https://medium.com/p/7814e154bd52"
+  },
+  {
+    date: "Sep 2017",
+    name: "Build an Online Photo Filter Application",
+    url: "https://medium.com/p/d6e00c36a3bb"
+  },
+  {
+    date: "Aug 2017",
+    name: "Build a flappy bird copy with TypeScript & Pixi.js",
+    url: "https://medium.com/p/890f5a07931e"
+  },
+  {
+    date: "Aug 2017",
+    name: "WebGL and Image Filter 101",
+    url: "https://medium.com/p/5017b290d02f"
+  },
+  {
+    date: "Jul 2016",
+    name: "Build a Tic Tac Toe with React Native",
+    url: "https://medium.com/p/2a3c44e697f2"
+  },
+  {
+    date: "Jul 2016",
+    name: "Let’s build a React Native application",
+    url: "https://medium.com/p/4190839a4f5c"
+  }
+];
