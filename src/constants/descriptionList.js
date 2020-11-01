9f81323640d7bdfd83aedbaf81906c479431ddb1
David Guan <davidguandev@gmail.com>
2020-11-01 12:03:08 +1100

    Tweak text

--- src/constants/descriptionList.js
+++ src/constants/descriptionList.js
@@ -1,19 +1,22 @@
 const CURRENT_POSITION = 'Engineer at Canva.';
 
-export default [{
+export default [
+  {
     text: CURRENT_POSITION,
     meta: {
       tagList: [
         {
-          tag: 'a',
-          start: CURRENT_POSITION.indexOf('Canva'),
-          end: CURRENT_POSITION.indexOf('Canva') + 'Canva'.length,
-          attributes: 'href="https://www.canva.com/" target="_blank" aria-label="canva, a company that provides graphic-design tools."'
+          tag: "a",
+          start: CURRENT_POSITION.indexOf("Canva"),
+          end: CURRENT_POSITION.indexOf("Canva") + "Canva".length,
+          attributes:
+            'href="https://www.canva.com/" target="_blank" aria-label="canva, a company that provides graphic-design tools."'
         }
       ]
     }
-  }, {
-    text: 'Programming machines to do web and graphics stuff.'
+  },
+  {
+    text: "Programming machines to do web and graphics stuff."
     // text: MAIN_EXPERIENCE,
     // meta: {
     //   tagList: [
@@ -44,7 +47,12 @@ export default [{
     //     }
     //   ]
     // }
-  }, {
-    text: 'Lifelong learner, book lover and competitive programming enthusiast.'
+  },
+  {
+    text:
+      "Lifelong learner, book lover and competitive programming enthusiast."
+  },
+  {
+    text: "Interested in accessibility and all kinds of things."
   }
-]
+];
